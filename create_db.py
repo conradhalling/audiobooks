dec9b62a4ee7fbfa1a40e80ed67592ce3c733058
conradhalling <conrad.halling@icloud.com>
2025-03-01 11:18:39 -0500

    feat: improve loading script
    create_db.py loads data into tbl_author, tbl_book, and
    tbl_book_author.
    
    Testing the foreign key constraints works.
    
    Loading the data more than once does not duplicate any
    data in the database.
    
    Require the use to set the path for the log file.
    
    Modify .gitignore to ignore files in the log subdirectory.

--- create_db.py
+++ create_db.py
@@ -7,8 +7,17 @@ EXAMPLE
     python3 create_db.py \
         --csv_file   data/audible.csv \
         --db_file    data/audiobooks.sqlite3 \
+        --log_file   log/create_db.log \
         --log_level  debug \
         --commit
+
+ASSUMPTION
+    Titles are unique.
+
+TESTS
+    -   Loading the data twice does not cause duplicated data.
+    -   Calling db_test_foreign_key_enforcement() raises a
+        sqlite3.IntegrityError exception.
 
 TO DO
     -   Add a column for translator.
@@ -25,16 +34,13 @@ import textwrap
 logger = logging.getLogger(__name__)
 
 
-def init_logging(log_level):
-    (long_base_name, extension) = os.path.splitext(__file__)
-    short_base_name = os.path.basename(long_base_name)
-    log_file_name = short_base_name + ".log"
+def init_logging(log_file, log_level):
     logging_numeric_level = getattr(logging, log_level.upper(), None)
     if not isinstance(logging_numeric_level, int):
         raise ValueError('Invalid log level: %s' % log_level)
-    format = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+    format = "[%(filename)s:%(lineno)4d - %(funcName)20s() ] %(levelname)s: %(message)s"
     logging.basicConfig(
-        filename=log_file_name,
+        filename=log_file,
         encoding='utf-8',
         format=format,
         level=logging_numeric_level)
@@ -43,12 +49,13 @@ def init_logging(log_level):
 def parse_args():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="Convert a domains CSV file to an HTML file",
+        description="Convert an audibooks CSV file to a SQLite database",
         epilog=textwrap.dedent(f"""
         Example:
           python3 {os.path.basename(__file__)} \\
             --csv_file   audible.csv \\
             --db_file    audiobooks.sqlite3 \\
+            --log_file   log/create_db.log \\
             --log_level  info \\
           [ --commit ]""")
     )
@@ -69,6 +76,11 @@ def parse_args():
         action="store_true",
     )
     parser.add_argument(
+        "--log_file",
+        help="output log file",
+        required=True,
+    )
+    parser.add_argument(
         "--log_level",
         choices=["debug", "info", "warning", "error", "critical"],
         help="logging level",
@@ -78,30 +90,220 @@ def parse_args():
     return args
 
 
-def save_author_data(cur, csv_file):
+##############################################################################
+# Database code.
+
+
+def db_create_tables(conn):
     logger.debug("Creating table tbl_author...")
-    sql03 = """
+    sql_create_tbl_author = """
         CREATE TABLE IF NOT EXISTS
         tbl_author
         (
             id INTEGER PRIMARY KEY,
-            name TEXT UNIQUE
+            name TEXT NOT NULL UNIQUE
         )"""
-    cur.execute(sql03)
+    conn.execute(sql_create_tbl_author)
 
-    sql04 = """
+    logger.debug("Creating table tbl_book...")
+    sql_create_tbl_book = """
+        CREATE TABLE IF NOT EXISTS
+        tbl_book
+        (
+            id INTEGER PRIMARY KEY,
+            title TEXT NOT NULL UNIQUE,
+            pub_date TEXT,
+            hours INTEGER NOT NULL,
+            minutes INTEGER NOT NULL
+        )
+    """
+    conn.execute(sql_create_tbl_book)
+
+    logger.debug("Creating tbl_book_author...")
+    sql_create_tbl_book_author = """
+        CREATE TABLE IF NOT EXISTS
+        tbl_book_author
+        (
+            id INTEGER PRIMARY KEY,
+            book_id INTEGER NOT NULL,
+            author_id INTEGER NOT NULL,
+            FOREIGN KEY (author_id) REFERENCES tbl_author(id),
+            FOREIGN KEY (book_id) REFERENCES tbl_book(id)
+        )
+    """
+    conn.execute(sql_create_tbl_book_author)
+
+
+def db_save_author(conn, author):
+    sql_select_author_id = """
         SELECT
             tbl_author.id
         FROM
             tbl_author
         WHERE
-            tbl_author.name = ?"""
-    sql05 = """
+            tbl_author.name = ?
+    """
+    sql_insert_author = """
         INSERT INTO tbl_author
         (
             name
         )
-        VALUES (?)"""
+        VALUES (?)
+    """
+    logger.debug(f"author: '{author}'")
+
+    # Is the author already in tbl_author?
+    cur = conn.execute(sql_select_author_id, (author,))
+    db_row = cur.fetchone()
+    logger.debug(f"author_id: {db_row}")
+    if db_row is not None:
+        author_id = db_row[0]
+        logger.debug(f"existing author_id: {author_id}")
+    else:
+        # Insert a new author.
+        logger.debug(f"Inserting '{author}' into tbl_author")
+        cur = conn.execute(sql_insert_author, (author,))
+        author_id = cur.lastrowid
+        logger.debug(f"new author_id: {author_id}")
+    logger.debug(f"author_id: {author_id}")
+    return author_id
+
+
+def db_save_book(conn, title, pub_date, hours, minutes):
+    sql_select_book_id = """
+        SELECT
+            tbl_book.id
+        FROM
+            tbl_book
+        WHERE
+            tbl_book.title = ?
+    """
+    sql_insert_book = """
+        INSERT INTO tbl_book
+        (
+            title,
+            pub_date,
+            hours,
+            minutes
+        )
+        VALUES (?, ?, ?, ?)
+    """
+    logger.debug(f"title: '{title}'")
+    logger.debug(f"pub_date: {pub_date}")
+    logger.debug(f"hours: {hours}")
+    logger.debug(f"minutes: {minutes}")
+
+    # Is the book already in tbl_book?
+    cur = conn.execute(sql_select_book_id, (title,))
+    db_row = cur.fetchone()
+    logger.debug(f"book_id: {db_row}")
+    if db_row is not None:
+        book_id = db_row[0]
+        logger.debug(f"existing book_id: {book_id}")
+    else:
+        # Insert a new book.
+        logger.debug(f"Inserting '{title}' into tbl_book...")
+        cur = conn.execute(sql_insert_book, (title, pub_date, hours, minutes))
+        book_id = cur.lastrowid
+        logger.debug(f"new book_id: {book_id}")
+    logger.debug(f"book_id: {book_id}")
+    return book_id
+
+
+def db_save_book_author(conn, book_id, author_id):
+    sql_select_book_author_id = """
+    SELECT
+        tbl_book_author.id
+    FROM
+        tbl_book_author
+    WHERE
+        tbl_book_author.book_id = ?
+        AND tbl_book_author.author_id = ?
+    """
+    sql_insert_book_author = """
+    INSERT INTO
+        tbl_book_author
+        (
+            book_id,
+            author_id
+        )
+        VALUES (?, ?)
+    """
+    logger.debug(f"book_id: {book_id}; author_id: {author_id}")
+
+    # Is the book_id-author_id combination already in tbl_book_author?
+    cur = conn.execute(sql_select_book_author_id, (book_id, author_id,))
+    db_row = cur.fetchone()
+    logger.debug(f"book_author_id: {db_row}")
+    if db_row is not None:
+        book_author_id = db_row[0]
+        logger.debug(f"existing book_author_id: {book_author_id}")
+    else:
+        logger.debug(f"Inserting book_id {book_id} and author_id {author_id} into tbl_book_author...")
+        cur = conn.execute(sql_insert_book_author, (book_id, author_id,))
+        book_author_id = cur.lastrowid
+        logger.debug(f"new book_author_id: {book_author_id}")
+    logger.debug(f"book_author_id: {book_author_id}")
+    return book_author_id
+
+
+def db_test_foreign_key_enforcement(conn):
+    # Attempt to delete a row from table authors.
+    # This should cause a foreign key constraint problem.
+    print("Attempting to violate foreign key constraint...")
+    sql_delete_author = """
+        DELETE from tbl_author
+        WHERE id = 1
+    """
+    try:
+        conn.execute(sql_delete_author)
+        print("  Foreign key constraint not enforced!")
+    except sqlite3.IntegrityError as exc:
+        print("  ", type(exc).__name__, ": ", exc, sep="")
+
+
+###############################################################################
+# CSV file processing code.
+
+def save_authors(conn, authors_str):
+    logger.debug(f"authors_str: '{authors_str}'")
+    authors = authors_str.split(sep="&")
+    logger.debug(f"authors: {authors}")
+    author_ids = []
+    for author in authors:
+        author = author.strip()
+        author_id = db_save_author(conn, author)
+        author_ids.append(author_id)
+    logger.debug(f"author ids: {author_ids}")
+    return author_ids
+
+
+def save_book(conn, title, pub_date, hours, minutes):
+    logger.debug(f"title: '{title}'")
+    logger.debug(f"pub_date: '{pub_date}'")
+    logger.debug(f"hours: {hours}")
+    logger.debug(f"minutes: {minutes}")
+    if pub_date == "":
+        pub_date = None
+    book_id = db_save_book(conn, title, pub_date, hours, minutes)
+    logger.debug(f"book_id: {book_id}")
+    return book_id
+
+
+def save_data(csv_file, db_file, commit_flag):
+    # Open a connection.
+    conn = sqlite3.connect(database=db_file, autocommit=True)
+
+    # Enforce foreign key constraints.
+    sql_pragma_foreign_keys = "PRAGMA foreign_keys = ON"
+    conn.execute(sql_pragma_foreign_keys)
+
+    # Begin an explicit transaction.
+    sql_begin_transaction = "BEGIN TRANSACTION"
+    conn.execute(sql_begin_transaction)
+
+    # Create tables.
+    db_create_tables(conn)
 
     with open(csv_file, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -109,92 +311,31 @@ def save_author_data(cur, csv_file):
         row = next(csv_reader)
         # Process the data lines. The author name is in column 1.
         # Multiple authors are separated by the "&" character.
+        # Save the tbl_author.id values for later use to associate
+        # to the book.
         for csv_row in csv_reader:
-            csv_author = csv_row[1]
-            logger.debug(f"csv_author: '{csv_author}'")
-            authors = csv_author.split(sep="&")
-            logger.debug(f"authors: {authors}")
-            for author in authors:
-                author = author.strip()
-                # Is the author already in tbl_author?
-                cur.execute(sql04, (author,))
-                db_row = cur.fetchone()
-                logger.debug(f"author_id: {db_row}")
-                if db_row is None:
-                    # Insert a new author.
-                    logger.debug(f"Inserting '{author}' into tbl_author")
-                    cur.execute(sql05, (author,))
+            # Insert one or more new authors into tbl_author.
+            csv_authors = csv_row[1]
+            logger.debug(f"csv_authors: '{csv_authors}'")
+            author_ids = save_authors(conn, csv_authors)
 
-def save_data(csv_file, db_file, commit_flag):
-    # Open a connection.
-    conn = sqlite3.connect(database=db_file, autocommit=True)
+            # Insert title into tbl_book.
+            csv_title = csv_row[0]
+            csv_pub_date = csv_row[2]
+            csv_hours = csv_row[3]
+            csv_minutes = csv_row[4]
+            logger.debug(f"csv_title: '{csv_title}'")
+            logger.debug(f"csv_pub_date: '{csv_pub_date}'")
+            logger.debug(f"hours: '{csv_hours}'")
+            logger.debug(f"minutes: '{csv_minutes}'")
+            book_id = save_book(conn, csv_title, csv_pub_date, csv_hours, csv_minutes)
 
-    # Enforce foreign key constraints.
-    sql01 = "PRAGMA foreign_keys = ON"
-    conn.execute(sql01)
+            # Insert book ID and associated author IDs into tbl_book_author.
+            for author_id in author_ids:
+                db_save_book_author(conn, book_id, author_id)
 
-    # Begin an explicit transaction.
-    sql02 = "BEGIN TRANSACTION"
-    conn.execute(sql02)
-
-    # Get a cursor.
-    cur = conn.cursor()
-
-    # Save author data.
-    save_author_data(cur, csv_file)
-
-    # logger.debug("Creating table tbl_book...")
-    # sql04 = """
-    #     CREATE TABLE IF NOT EXISTS
-    #     tbl_book
-    #     (
-    #         id INTEGER PRIMARY KEY,
-    #         title TEXT,
-    #         author_id INTEGER,
-    #         FOREIGN KEY(author_id) REFERENCES tbl_author(id)
-    #     )"""
-    # cur.execute(sql04)
-
-    # books_list = [
-    #     ("Dawn", "Octavia", "Butler"),
-    #     ("Foundation", "Isaac", "Asimov"),
-    #     ("Ancillary Justice", "Anne", "Leckie"),
-    # ]
-    # sql06 = """
-    #     SELECT
-    #         id
-    #     FROM
-    #         authors
-    #     WHERE
-    #         first_name = ?
-    #         AND last_name = ?"""
-    # sql07 = """
-    #     INSERT INTO
-    #     books
-    #     (
-    #         title,
-    #         author_id
-    #     )
-    #     VALUES (?, ?)"""
-
-    # for row in books_list:
-    #     cur.execute(sql06, (row[1], row[2]))
-    #     author_id = cur.fetchone()[0]
-    #     cur.execute(sql07, (row[0], author_id))
-    # print("  Done.")
-
-    # # Attempt to delete a row from table authors.
-    # # This should cause a foreign key constraint problem.
-    # print("Attempting to violate foreign key constraint...")
-    # sql08 = """
-    #     DELETE from authors
-    #     WHERE id = 1
-    # """
-    # try:
-    #     cur.execute(sql08)
-    #     print("  Foreign key constraint not enforced!")
-    # except sqlite3.IntegrityError as exc:
-    #     print("  ", type(exc).__name__, ": ", exc, sep="")
+    # Test enforcement of foreign key constraints.
+    db_test_foreign_key_enforcement(conn)
 
     # Commit or roll back database changes. If the rollback is successful, the
     # size of the database file will be 0 bytes.
@@ -210,7 +351,6 @@ def save_data(csv_file, db_file, commit_flag):
         print(f"  The file {db_file} should be empty.")
     
     # Clean up.
-    cur.close()
     conn.close()
 
 def main():
@@ -219,7 +359,7 @@ def main():
     if sys.version_info.major != 3 or sys.version_info.minor < 12:
         raise ValueError("This script requires Python >= 3.12.")
     args = parse_args()
-    init_logging(args.log_level)
+    init_logging(args.log_file, args.log_level)
     save_data(args.csv_file, args.db_file, args.commit)
 
 if __name__ == "__main__":
